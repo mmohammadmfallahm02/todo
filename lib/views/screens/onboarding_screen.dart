1cef6647e53cd1e4648ebf1e8e08dee7a54ccd9a
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-15 13:30:19 +0330

    add onboarding appBar widget

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:getx_task_manager/utils/colors.dart';
+import 'package:getx_task_manager/utils/style.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      backgroundColor: AppColor.darkBackgroundAppColor,
+      appBar: const OnBoardingAppBar(),
     );
   }
+}
+
+class OnBoardingAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const OnBoardingAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          children: [
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'SKIP',
+                  style: MyDefaultTextStyle.getRegularStyle(
+                      color: AppColor.darkSecondaryTextButtonAppColor,
+                      fontSize: FontSize.c16),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
 }
