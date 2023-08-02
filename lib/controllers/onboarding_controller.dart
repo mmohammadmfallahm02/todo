b0fe89fe00998383f21e42dcb11b86322900d271
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-08-02 20:40:59 +0330

    add smoothPageIndicator to onboarding screen

--- lib/controllers/onboarding_controller.dart
+++ lib/controllers/onboarding_controller.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_task_manager/constant/app_text.dart';
 import 'package:getx_task_manager/gen/assets.gen.dart';
 import 'package:getx_task_manager/models/onboarding_pageview_model.dart';
 
 class OnboardingController extends GetxController {
+  // <--------------------------variables-------------------------->
+  RxString btnText = 'NEXT'.obs;
+  RxDouble btnBackOpacity = 1.0.obs;
+  final PageController pageController = PageController();
+
   final List<PageData> pages = [
     PageData(
       image: Assets.images.onBoardingPic1,
@@ -21,11 +27,36 @@ class OnboardingController extends GetxController {
       description: AppTexts.onBoardingDescription3,
     ),
   ];
-  // List<PageData> get pages => PageViewData.pages;
-}
 
-// class PageViewData {
-//   static List<PageData> pages = [
-//     // Add more PageData objects for additional pages
-//   ];
-// }
+  // <--------------------------methods-------------------------->
+
+  // button NEXT to move between pages
+  void goNextPage() {
+    if (pageController.page!.toInt() < pages.length - 1) {
+      pageController.animateToPage(pageController.page!.toInt() + 1,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+    } else {}
+  }
+
+  // button BACK to move between pages
+  void goBackPage() {
+    if (pageController.page!.toInt() > 0) {
+      pageController.animateToPage(pageController.page!.toInt() - 1,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+    }
+  }
+
+// change button text with change pageView page
+  onPageChange(int index) {
+    if (index == 0) {
+      btnBackOpacity.value = 0;
+    } else {
+      btnBackOpacity.value = 1;
+      if (index == pages.length - 1) {
+        btnText.value = 'START';
+      } else {
+        btnText.value = 'NEXT';
+      }
+    }
+  }
+}
--- lib/views/screens/onBoarding/onboarding_screen.dart
+++ lib/views/screens/onBoarding/onboarding_screen.dart
@@ -6,6 +6,7 @@ import 'package:getx_task_manager/components/colors.dart';
 import 'package:getx_task_manager/components/size_config.dart';
 import 'package:getx_task_manager/components/style.dart';
 import 'package:getx_task_manager/views/screens/onBoarding/components/onboarding_appBar.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends GetView<OnboardingController> {
   const OnboardingScreen({super.key});
@@ -20,57 +21,108 @@ class OnboardingScreen extends GetView<OnboardingController> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            // top area
             SizedBox(
               height: SizeConfig.screenHeight * 0.66,
-              child: PageView.builder(
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  final pageItem = controller.pages[index];
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SvgPicture.asset(
-                        pageItem.image,
-                        height: SizeConfig.screenHeight * 0.4,
+              child: Stack(
+                children: [
+                  // page view
+                  PageView.builder(
+                    onPageChanged: (value) => controller.onPageChange(value),
+                    controller: controller.pageController,
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      final pageItem = controller.pages[index];
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SvgPicture.asset(
+                            pageItem.image,
+                            height: SizeConfig.screenHeight * 0.38,
+                          ),
+                          const SizedBox(),
+                          Text(
+                            pageItem.title,
+                            style: MyDefaultTextStyle.getBoldStyle(
+                                fontSize: FontSize.c32,
+                                color: AppColor.lightPrimaryTextButtonAppColor),
+                          ),
+                          Text(
+                            pageItem.description,
+                            textAlign: TextAlign.center,
+                            style: MyDefaultTextStyle.getRegularStyle(
+                                fontSize: FontSize.c16,
+                                color: AppColor.lightPrimaryTextButtonAppColor),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+
+                  // indicator
+                  Positioned(
+                    top: SizeConfig.screenHeight / 2.45,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 100,
+                      child: SmoothPageIndicator(
+                        controller: controller.pageController,
+                        count: controller.pages.length,
+                        axisDirection: Axis.horizontal,
+                        effect: WormEffect(
+                          spacing: 8.0,
+                          radius: 10.0,
+                          dotWidth: SizeConfig.screenWidth / 7,
+                          dotHeight: 3.5,
+                          paintStyle: PaintingStyle.stroke,
+                          strokeWidth: 1.5,
+                          dotColor: Colors.grey,
+                          activeDotColor: AppColor.primaryAppColor,
+                          type: WormType.underground,
+                        ),
                       ),
-                      const SizedBox(),
-                      Text(
-                        pageItem.title,
-                        style: MyDefaultTextStyle.getBoldStyle(
-                            fontSize: FontSize.c32,
-                            color: AppColor.lightPrimaryTextButtonAppColor),
-                      ),
-                      Text(
-                        pageItem.description,
-                        textAlign: TextAlign.center,
-                        style: MyDefaultTextStyle.getRegularStyle(
-                            fontSize: FontSize.c16,
-                            color: AppColor.lightPrimaryTextButtonAppColor),
-                      ),
-                    ],
-                  );
-                },
+                    ),
+                  )
+                ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'BACK',
-                      style: MyDefaultTextStyle.getRegularStyle(
-                          color: AppColor.lightSecondaryTextButtonAppColor,
-                          fontSize: FontSize.c16),
-                    )),
-                ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'NEXT',
-                      style: MyDefaultTextStyle.getRegularStyle(
-                          color: Colors.white, fontSize: FontSize.c16),
-                    )),
-              ],
+
+            // btn area
+            Obx(
+              () => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // back btn
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 100),
+                    opacity: controller.btnBackOpacity.value,
+                    child: TextButton(
+                        onPressed: () {
+                          controller.goBackPage();
+                        },
+                        child: Text(
+                          'BACK',
+                          style: MyDefaultTextStyle.getRegularStyle(
+                              color: AppColor.lightSecondaryTextButtonAppColor,
+                              fontSize: FontSize.c16),
+                        )),
+                  ),
+
+                  // next btn
+                  ElevatedButton(
+                      onPressed: () {
+                        controller.goNextPage();
+                      },
+                      child: Text(
+                        controller.btnText.value,
+                        style: MyDefaultTextStyle.getRegularStyle(
+                            color: Colors.white, fontSize: FontSize.c16),
+                      )),
+                ],
+              ),
             ),
           ],
         ),
